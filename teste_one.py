d2aee543c2be127aac18f7573a740ffc69b6642e
Paulo Sérgio <paulo.sacramento@oi.net.br>
2018-04-12 12:18:30 -0300

    Fix scripts

--- teste_one.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import pandas as pd
-cct = pd.read_csv('circuits.csv', encoding='latin1')
-drivers = pd.read_csv('drivers.csv', encoding='latin1')
-dStands = pd.read_csv('driverStandings.csv', encoding='latin1')
-group = dStands[['driverId','points']].groupby('driverId',sort=True).mean()
-print(group)
-res = group[['points']].apply(lambda x: x.order(ascending=False).head(3))
-print(res)
