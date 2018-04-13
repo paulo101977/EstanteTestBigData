14b827d81098f719015186badabe6a2a72801961
Paulo Sérgio Galdino Sacramento <paulo10.1977@yahoo.com.br>
2018-04-13 11:53:21 -0300

    Update pitstop.py

--- scripts/pitstop.py
+++ scripts/pitstop.py
@@ -5,42 +5,63 @@ import pandas as pd
 races = pd.read_csv('races.csv')
 
 # read constructor info
-constructors = pd.read_csv('constructorResults.csv')
+constructor = pd.read_csv('constructors.csv')
+cons_result = pd.read_csv('constructorResults.csv')
+constructor = pd.merge(constructor, cons_result, on='constructorId')
 
 # read driver info
 drivers = pd.read_csv('drivers.csv', encoding="latin1")
 
-#print(constructors)
 
 pitstop = pd.read_csv('pitStops.csv')
 
 
-df = (
-    pd.merge(races, pitstop, on='raceId')
+#merge with constructor info
+pitstop = pd.merge(pitstop, constructor, on='raceId')
+
+
+#print(pitstop)
+
+# get race info
+df = pd.merge(races, pitstop, on='raceId')
+
+#print(df)
+
+
+df = ( 
+    df
         .groupby([ 'milliseconds'])
         .min()
-        .groupby('name')
-        .head()[['name', 'year', 'driverId']]
+        .groupby('name_x')
+        .head()[['name_x', 'year', 'driverId', 'name_y']]
         .reset_index()
 )
 
 
+# get racers info (name)
 df = pd.merge(df, drivers , on='driverId')
 
+# pilot name
 df['pilot'] = df.apply(lambda x : x['forename'] + ' ' + x['surname'], axis=1)
 
+#print(df)
+
 df = df.sort_values('year', ascending=False)
+
+df = df[['year', 'milliseconds', 'name_x', 'pilot', 'name_y']]
 
 #rename columns
 df = df.rename(
     columns={
-        'year': 'Temporada:',
-        'milliseconds': 'Pit stop mili:',
-        'name': 'Corrida:',
-        'pilot': 'Piloto'
+        'year': 'Temporada:', 
+        'milliseconds': 'Pit stop mili:', 
+        'name_x': 'Corrida:',
+        'pilot': 'Piloto:',
+        'name_y': 'Equipe:'
     }
 )
 
+print('saving...')
 writer = pd.ExcelWriter('pitstop.xlsx')
-racesFromYears.to_excel(writer,'Sheet1')
+df.to_excel(writer,'Sheet1')
 writer.save()
